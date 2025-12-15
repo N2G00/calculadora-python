c4a3b846846c53b24504accf665e2c9a71a89237
yowyow <brahim@MacBook-Air.local>
2025-12-15 11:48:01 +0100

    Prueba de seguridad: Intento de push directo

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -1,5 +1,15 @@
+from src.calculator import add, subtract, divide
+import pytest
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
