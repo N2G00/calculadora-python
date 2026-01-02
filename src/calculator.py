0efc55b1d87c5f1e7c163ecec749bd73e2b68e2a
Najib <najibbahij@gmail.com>
2026-01-02 12:24:33 +0100

    uncoment function divide

--- src/calculator.py
+++ src/calculator.py
@@ -7,8 +7,7 @@ def subtract(a, b):
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
-    #return a / b
-    #comment
+    return a / b
 def sq(n):
    return n * n
 
